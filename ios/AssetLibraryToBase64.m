aea96ca32f15b5ab16f473130b417976d473f61c
Tom Krones <tkrones@gmail.com>
2016-01-22 22:23:45 +1300

    and resize the image to fit in a 2048x2048 box

--- ios/AssetLibraryToBase64.m
+++ ios/AssetLibraryToBase64.m
@@ -25,9 +25,31 @@ RCT_EXPORT_METHOD(readImage:(NSString *)input callback:(RCTResponseSenderBlock)c
     // and turn it into a bitmap using the CGImageRef opaque type.
     ALAssetRepresentation *representation = [asset defaultRepresentation];
     CGImageRef imageRef = [representation fullResolutionImage];
+    UIImage *sourceImage = [UIImage imageWithCGImage:imageRef];
+
+    // Scale image to fit in 2048x2048
+    CGSize maxSize = CGSizeMake(2048, 2048);
+    CGSize scaledSize = maxSize;
+    float scaleFactor = 1.0;
+    if( sourceImage.size.width > sourceImage.size.height ) {
+        scaleFactor = sourceImage.size.width / sourceImage.size.height;
+        scaledSize.width = maxSize.width;
+        scaledSize.height = maxSize.height / scaleFactor;
+    }
+    else {
+        scaleFactor = sourceImage.size.height / sourceImage.size.width;
+        scaledSize.height = maxSize.height;
+        scaledSize.width = maxSize.width / scaleFactor;
+    }
+
+    UIGraphicsBeginImageContextWithOptions( scaledSize, NO, 0.0 );
+    CGRect scaledImageRect = CGRectMake( 0.0, 0.0, scaledSize.width, scaledSize.height );
+    [sourceImage drawInRect:scaledImageRect];
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
 
     // Create UIImageJPEGRepresentation from CGImageRef
-    NSData *imageData = UIImageJPEGRepresentation([UIImage imageWithCGImage:imageRef], 1.0);
+    NSData *imageData = UIImageJPEGRepresentation(scaledImage, 1.0);
 
     // Convert to base64 encoded string
     NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
@@ -37,8 +59,6 @@ RCT_EXPORT_METHOD(readImage:(NSString *)input callback:(RCTResponseSenderBlock)c
   } failureBlock:^(NSError *error) {
     NSLog(@"that didn't work %@", error);
   }];
-
-
-
 }
+
 @end
